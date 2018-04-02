80f6348dcf476a58d5431e10ea0c4508b9ff0812
sladekm <sladek.matyas@seznam.cz>
2018-04-02 20:13:50 +0200

    Implementace faktoriálu a oprava testů faktoriálu

--- src/math/Number.h
+++ src/math/Number.h
@@ -4,15 +4,14 @@
  * @date     26.3.18
  * @author   Adam Mátl <xmatla00@stud.fit.vutbr.cz>
  * @author   Matyáš Sládek <xslade21@stud.fit.vutbr.cz>
- *
+ * 
  * @brief    Hlavičkový soubor pro Number z matematické knihovny
  */
 
-#ifndef FIT_IVS_PROJECT2_NUMBER_H
-#define FIT_IVS_PROJECT2_NUMBER_H
-
-#include <cmath>    //Used for isnan()
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <complex>
+#include <limits>
 #include <iostream>
 
 namespace team22::Math
@@ -28,6 +27,14 @@ private:
     std::complex<double> value;
 
 	explicit operator std::complex<double>() const {return value;}
+
+	/**
+	 * @details Gamma funkce implementovaná pomocí Lanczosovy aproximace
+	 * 
+	 * @param z vstupní operand
+	 * @return gamma čísla z
+	 */
+	std::complex<double> gamma(std::complex<double> z);
 
     /**
      * @return True pokud je některý z parametrů Nan, jiank False
@@ -60,7 +67,7 @@ public:
 
     /**
      * @brief Konstruktor
-     *
+     * 
      * @param real Reálná část
      * @param imaginary Imaginární část
      */
@@ -68,14 +75,14 @@ public:
 
     /**
      * @brief Konstruktor
-     *
+     * 
      * @param other Komplexní číslo
      */
     Number(const std::complex<double>& other);
 
     /**
      * @brief Sčítání
-     *
+     * 
      * @param addend sčítanec
      * @return součet
      * @throws UndefinedException pokud je některá z částí operandů nan nebo pokud jsou obě části operandů nekonečno a zároveň operandy nejsou ze stejných kvadrantů komplexní roviny
@@ -83,13 +90,13 @@ public:
     Number add(Number addend) const;
 
     /**
-	 * @see add
+	 * @see ::add
 	 */
     Number operator+(const Number &number) const;
 
     /**
      * @brief Odečítání
-     *
+     * 
      * @param subtrahend menšitel
      * @return rozdíl
      * @throws UndefinedException pokud je některá z částí operandů nan nebo pokud jsou obě části operandů nekonečno a zároveň operandy nejsou z opačných kvadrantů komplexní roviny
@@ -97,13 +104,13 @@ public:
     Number sub(Number subtrahend) const;
 
     /**
-	 * @see sub
+	 * @see ::sub
 	 */
     Number operator-(const Number &number) const;
 
     /**
      * @brief Násobení
-     *
+     * 
      * @param multiplier činitel
      * @return součin
      * @throws UndefinedException pokud je některá z částí operandů nan nebo nekonečno
@@ -111,41 +118,41 @@ public:
     Number mul(Number multiplier) const;
 
     /**
-	 * @see mul
+	 * @see ::mul
 	 */
     Number operator*(const Number &number) const;
 
     /**
      * @brief Dělení
-     *
+     * 
      * @param divisor dělitel
      * @return rozdíl
-     * @throws UndefinedException TODO
+     * @throws UndefinedException Pokud je některá část operandů nan nebo obě části obou operandů jsou nekonečno nebo je dělitel nulový a obě části dělence nejsou nekonečno a reálná část dělence není pozitivní nekonečno a imaginární část dělence není nekonečno
      */
     Number div(Number divisor) const;
 
     /**
-	 * @see div
+	 * @see ::div
 	 */
     Number operator/(const Number &number) const;
 
     /**
      * @brief Umocňování
-     *
+     * 
      * @param exponent mocnitel
      * @return mocnina
-     * @throws UndefinedException TODO
+     * @throws UndefinedException Pokud jsou operandy mimo definiční obor mocniny
      */
     Number pow(Number exponent) const;
 
     /**
-	 * @see pow
+	 * @see ::pow
 	 */
     Number operator^(const Number &number) const;
 
     /**
      * @brief Odmocňování
-     *
+     * 
      * @param degree odmocnitel
      * @return odmocnina
      * @throws UndefinedException pokud je některá z částí operandů nan nebo nekonečno nebo pokud je některá z částí odmocněnce nenulová a odmocnitel je nulový
@@ -154,7 +161,7 @@ public:
 
     /**
      * @brief Zbytek po dělení
-     *
+     * 
      * @param divisor dělitel
      * @return zbytek
      * @throws UndefinedException pokud je některá z částí operandů nan nebo nekonečno nebo pokud je dělitel nulový
@@ -162,21 +169,22 @@ public:
     Number mod(Number divisor) const;
 
     /**
-	 * @see mod
+	 * @see ::mod
 	 */
     Number operator%(const Number &number) const;
 
     /**
      * @brief Faktoriál
-     *
+     * 
      * @return faktoriál
+     * @throws UndefinedException pokud je některá z částí operandů nan nebo jsou obě části operandů nekonečno nebo je reálná část operandu záporné nekonečno
      */
-    Number fact() const;
+    Number fact();
 
-    Number operator!() const;
-
-    bool operator==(const Number &rhs) const;
-    bool operator!=(const Number &rhs) const;
+    /**
+     * @see ::fact
+     */
+    Number operator!();
 
     /**
      * @details Vypíše komplexní číslo ve tvaru: reálná_část +/- imaginární_část. Pokud je některá z částí 0, není vypsána, pokud jsou 0 části obě, vypíše 0.
@@ -186,5 +194,3 @@ public:
 };
 
 };
-
-#endif //FIT_IVS_PROJECT2_NUMBER_H
