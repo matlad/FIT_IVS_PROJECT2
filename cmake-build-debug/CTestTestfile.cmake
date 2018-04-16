695104baab2f4d8e3df24168d18ff96eeb37af9d
matla <matla@matla.cz>
2018-04-16 21:56:56 +0200

    Clean gh-pages branch

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/Linux_Data/Code/school/FIT_IVS_PROJECT2
-# Build directory: /media/Linux_Data/Code/school/FIT_IVS_PROJECT2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest-build")
--- cmake-build-debug/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/Linux_Data/Code/school/FIT_IVS_PROJECT2/cmake-build-debug/googletest-src/googletest
-# Build directory: /media/Linux_Data/Code/school/FIT_IVS_PROJECT2/cmake-build-debug/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
