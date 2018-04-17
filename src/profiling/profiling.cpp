5f391ec9af925dba1afe0c48694ed0370a473b6f
Jb535 <jack.mitura@gmail.com>
2018-04-17 15:38:21 +0200

    Upravy a zmena adresaru

--- src/profiling/profiling.cpp
+++ src/profiling/profiling.cpp
@@ -3,41 +3,43 @@
 #include <string>
 #include <cstdio>
 #include <vector>
-#include "../src/math/Number.h"
+#include "../math/Number.h"
 
 using namespace team22::Math;
 
-int main(int argc, char** argv){
+int main(int argc, char **argv){
 	
 	std::string in;										//vstupni hodnota
-	Number mean = {0};									//prumer
+	//Number mean = {0};									//prumer
 	std::vector<Number> inValues;						//pole vstupnich hodnot
-	
-	std::ifstream insert( argv[1] );					//otevreni souboru
-	
-	if(insert.is_open()){								//kontrola zdali je soubor otevren
-			while(std::getline(insert,in)){				//nacteni hodnot ze vstupu
-				inValues.push_back(std::stod(in));		//presun hodnoty na misto v poli
-		}
-		insert.close();									//zavreni souboru
-	}
-
 	Number sum = {0};
 	Number average = {0};
-
-	//vypocet prumeru
-	for(auto value : inValues){
-		sum = sum + Number(value);
-	}
-	average = sum/Number(inValues.size());
-
 	Number sampleSD = {0};
 	Number sq = {2};
 
-	//vypocet vyberove smerodatne odchylky
-	for(auto value: inValues){
-    	sampleSD = sampleSD + Number((value - mean)^sq);
+	std::ifstream insert ( argv[1] );					//otevreni souboru
+	
+	if (insert.is_open()) {								//kontrola zdali je soubor otevren
+			while (std::getline(insert,in)) 			//nacteni hodnot ze vstupu
+				inValues.push_back(std::stod(in));		//presun hodnoty na misto v poli
+		
+		insert.close();									//zavreni souboru
 	}
+	
+	/**
+	 * vypocet prumeru
+	 */
+	for (auto value : inValues) 
+		sum = sum + Number(value);
+	
+	average = sum/Number(inValues.size());
+
+	/**
+	 * vypocet vyberove smerodatne odchylky
+	 */
+	for (auto value: inValues)
+    	sampleSD = sampleSD + Number((value - average)^sq);
+	
 	sampleSD = sampleSD/Number(inValues.size() - 1);
 	sampleSD = sampleSD^Number(0.5);
 
