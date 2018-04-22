84d43f4b22910c23d0aadcee7f43b384dffe7a14
matla <matla@matla.cz>
2018-04-22 16:36:09 +0200

    oprava generování dokumentace

---
+++ src/install_dep.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## Cmake
+#################
+wget https://cmake.org/files/v3.10/cmake-3.10.3-Linux-x86_64.sh
+chmod +x cmake-3.10.3-Linux-x86_64.sh
+sudo ./cmake-3.10.3-Linux-x86_64.sh
+
+## g++
+##################
+
+## Qt
+##################
+sudo apt-get install qt-5
+
